74598ea4dc2737b95446ffe93982adcaf0b3126f
Deepak Singh <69311746+deepaksinghdk@users.noreply.github.com>
2021-08-25 21:41:03 +0530

    Add files via upload

---
+++ src/com/javadsa/LinearSearch.java
@@ -0,0 +1,30 @@
+//Linear Search program in java
+package com.javadsa;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        //initialising the array
+        int[] arrs ={32,34,4,55,743,98,72,01,37,54,71,376};
+        //setting a target value to search for
+        int target =7463;
+        //calling the funtion and storing it to a variable to print it
+        int ans = searchAlgo(arrs , target);
+        System.out.println(ans);
+
+    }
+    static int searchAlgo(int[] arr , int target){
+        //checking if the array is not null
+        if(arr.length == 0){
+            return -1;
+        }
+        //for loop for irritating all the  value in the array one by one linearly
+        for(int index = 0 ; index < arr.length ; index++){
+            int element = arr[index];
+            if(element == target){
+                return index;
+            }
+        }
+        //if the target is not present in the array
+    return -1;
+    }
+}
